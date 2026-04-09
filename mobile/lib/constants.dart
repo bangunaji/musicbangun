4cc8539515d114f8ed3d83c664bd8114c69c2ecc
bangunaji <alfahriaji9@gmail.com>
2026-04-09 16:14:20 +0700

    Config: Connect mobile app to backend at 192.168.1.4

--- mobile/lib/constants.dart
+++ mobile/lib/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   // Ganti 'localhost' atau '10.0.2.2' dengan alamat IP komputer Anda
   // Contoh: 'http://192.168.1.5:3000'
   // Ketik 'ipconfig' di terminal/command prompt Windows untuk mencari IP Anda (IPv4 Address)
-  static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'http://192.168.1.4:3000';
 }
